faa6840a8c506f557f0b6da51780d38c59ed00d7
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-03-31 17:42:12 +0500

    Initial Commit

---
+++ Predictions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Predictions
+//
+//  Created by Алексей Пархоменко on 31.03.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
